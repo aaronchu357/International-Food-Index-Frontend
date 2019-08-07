b4c6281668a3ac080007188546eb366c64d310c3
Aaron Chu <aaronchu357@gmail.com>
2019-08-07 12:29:54 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import FirstContainer from './containers/FirstContainer'
 
-class App extends React.Component {
-  render() {
-    return (
-      <FirstContainer />
-    );
-  }
+const App = () => {
+  return (
+    <FirstContainer />
+  )
 }
 
 export default App
